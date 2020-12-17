ea44036730292824ddf00d037ae1f3cf2407115e
Santosh Kumar <sntshkmr60@gmail.com>
2020-12-17 20:49:52 +0530

    Fix the hand reset glitch

--- clock/script.js
+++ clock/script.js
@@ -1,6 +1,7 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+const allHands = document.querySelectorAll('.hand');
 
 function setDate() {
   const now = new Date();
@@ -10,6 +11,12 @@ function setDate() {
   const secondsDegrees = (seconds / 60) * 360 + 90;
   const minutesDegrees = (minutes / 60) * 360 + 90;
   const hoursDegrees = (hours / 12) * 360 + 90;
+
+  if (secondsDegrees === 90) {
+    allHands.forEach(hand => hand.style.transition = 'none')
+  } else {
+    allHands.forEach(hand => hand.style.transition = '')
+  }
 
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
