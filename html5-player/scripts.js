e682650e5208175ec68b4af3ace7b21303b8a427
Santosh Kumar <sntshkmr60@gmail.com>
2020-12-31 00:30:52 +0530

    HTML5 Player: Make skip button work

--- html5-player/scripts.js
+++ html5-player/scripts.js
@@ -4,8 +4,8 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 
 // Build out functions
@@ -22,6 +22,9 @@ function updateButton() {
   toggle.textContent = icon;
 }
 
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
 
 // Hook up the event listeners
 video.addEventListener('click', togglePlay);
@@ -30,3 +33,4 @@ video.addEventListener('pause', updateButton);
 
 
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
