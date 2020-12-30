85cdf55192e5409036094002c2128ffe7e12021d
Santosh Kumar <sntshkmr60@gmail.com>
2020-12-31 01:39:59 +0530

    HTML5 Player: Make seeking bar update

--- html5-player/scripts.js
+++ html5-player/scripts.js
@@ -30,10 +30,16 @@ function handleRangeUpdate() {
   video[this.name] = this.value;
 }
 
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
 // Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 
 toggle.addEventListener('click', togglePlay);
