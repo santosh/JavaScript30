0f9df69581324b3ad90833f755e0ff40c1d3ebe7
Santosh Kumar <sntshkmr60@gmail.com>
2020-12-31 01:29:46 +0530

    HTML5 Player: Make volume and range slider work

--- html5-player/scripts.js
+++ html5-player/scripts.js
@@ -26,6 +26,10 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
 // Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -34,3 +38,5 @@ video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate))
