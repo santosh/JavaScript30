d08da95d6e57cc13eb3d164578716600c5a882ba
Santosh Kumar <sntshkmr60@gmail.com>
2021-02-17 21:41:59 +0530

    Webcam: greenScreen - extract alpha and extract colors

--- webcam/scripts.js
+++ webcam/scripts.js
@@ -28,6 +28,7 @@ function paintToCanvas() {
     let pixels = ctx.getImageData(0, 0, width, height);
     // pixels = redEffect(pixels);
     // pixels = rgbSplit(pixels);
+    pixels = greenScreen(pixels);
 
     // ghost effect
     // ctx.globalAlpha = 0.2;
@@ -66,6 +67,36 @@ function rgbSplit(pixels) {
   }
   return pixels;
 }
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (
+      red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
+
 
 getVideo();
 
