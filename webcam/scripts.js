95bba12c17070bfe62f6d81e9bccf169949a91f6
Santosh Kumar <sntshkmr60@gmail.com>
2021-02-17 20:10:48 +0530

    Webcam: Adds redEffect filter

--- webcam/scripts.js
+++ webcam/scripts.js
@@ -25,6 +25,9 @@ function paintToCanvas() {
 
   setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    pixels = redEffect(pixels);
+    ctx.putImageData(pixels, 0, 0);
   }, 32);
 }
 
@@ -40,6 +43,15 @@ function takePhoto() {
   link.setAttribute('download', 'handsome');
   link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
   strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100;
+    pixels.data[i + 1] = pixels.data[i + 1] - 25;
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+  }
+  return pixels;
 }
 
 getVideo();
