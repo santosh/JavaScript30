f8e4f527835b8d0f8e4cd97c75b58e5f38b61d75
Santosh Kumar <sntshkmr60@gmail.com>
2021-02-17 19:40:49 +0530

    Webcam: Paint video media stream to canvas

--- webcam/scripts.js
+++ webcam/scripts.js
@@ -8,12 +8,24 @@ function getVideo() {
   navigator.mediaDevices
     .getUserMedia({ video: true, audio: false })
     .then((localMediaStream) => {
+      console.log(localMediaStream);
       video.srcObject = localMediaStream;
       video.play();
     })
     .catch((err) => {
       console.log(`Oh No!!!`, err);
     });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 32);
 }
 
 getVideo();
