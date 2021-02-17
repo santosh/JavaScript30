2a9b1f03216718c1d0f0ad8b9c69279dfdef7e0a
Santosh Kumar <sntshkmr60@gmail.com>
2021-02-17 19:33:42 +0530

    Webcam: Get video feed via navigator.mediaDevices.getUserMedia

--- webcam/scripts.js
+++ webcam/scripts.js
@@ -3,3 +3,17 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch((err) => {
+      console.log(`Oh No!!!`, err);
+    });
+}
+
+getVideo();
